746880a6d12f222aeebfb39240b9a1e7e9ede1ca
Hassan Sadiq Khan <hassansadiqkhan8@gmail.com>
2025-01-22 14:04:19 +0500

    rendering products in cartand checkout templates

--- store/views.py
+++ store/views.py
@@ -2,14 +2,29 @@ from django.shortcuts import render
 from .models import *
 
 
-
 def store(request):
     products = Product.objects.all()
 
     return render(request, 'store/store.html', {"products": products})
 
 def cart(request):
-    return render(request, 'store/cart.html', {})
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_cart_total":0, "get_cart_items":0}
+
+    return render(request, 'store/cart.html', {"items": items, "order": order})
 
 def checkout(request):
-    return render(request, 'store/checkout.html', {})
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_cart_total":0, "get_cart_items":0}
+
+    return render(request, 'store/checkout.html', {"items": items, "order": order})
